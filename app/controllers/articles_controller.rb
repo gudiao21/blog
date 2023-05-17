9afd434bbd354be1f608119cee7c3669459723ec
gudiao21 <lucianodantas53@gmail.com>
2023-05-17 17:14:38 -0300

    Adicionar arquivos da pasta '/blog'

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,12 @@
 class ArticlesController < ApplicationController
   def index
+    @articles = Article.all
+  end
+
+  #Por padrão, a Action 'show' renderizará 'app/views/articles/show.html.erb':
+  def show
+    #'params[:id]' tem que ter sido mencionado em 'routes.rb', em: get"/articles/:id", to: "articles#show"
+    #A Action 'show' chama 'Article.find' com o 'id' capturado pelo parâmetro de rota. O 'article' retonado é armazenado na variável de instância '@article', portanto, é acessível pela visualização. Por padrão, a action 'show renderizará 'app/views/articles/show.html.erb'.
+    @article = Article.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,6 @@ Rails.application.routes.draw do
   # root "articles#index"
   #"get '/articles'" é mapeada para(to) a ação 'index' de 'ArticlesController':
   get "/articles", to: "articles#index"
+  #Essa 'route'abaixo é outra rota 'get', mas tem algo extra sem seu 'path', o ':id'.Isso designa um parâmetro de rota.Um parâmetro de rota captura um segmento do caminho da solicitação e coloca esse valor no 'Params' Hash, que é acessível pela ação do controlador.Por exemplo, ao lidar com uma solicitação como 'Get http://localhost:3000/articles/1', '1' seria capturado como o valor para ':id', que seria acessível como 'params[:id] na action show de ArticlesContoller.
+  get"/articles/:id", to: "articles#show"
 end
