b452b3191cd062f38902a0e77d32dfad78d4739a
gudiao21 <lucianodantas53@gmail.com>
2023-05-17 19:29:43 -0300

    Adicionar arquivos da pasta '/blog'

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,4 +9,8 @@ class ArticlesController < ApplicationController
     #A Action 'show' chama 'Article.find' com o 'id' capturado pelo parâmetro de rota. O 'article' retonado é armazenado na variável de instância '@article', portanto, é acessível pela visualização. Por padrão, a action 'show renderizará 'app/views/articles/show.html.erb'.
     @article = Article.find(params[:id])
   end
+  #Agora passamos para o 'C'(Create) do CRUD. Normalmente, em aplicativos da Web, a criação de um novo recurso é um processo de várias etapas. Primeiro, o usuário solicita um formulário para preencher. Em seguida, o usuário envia o formulário. Se não houver erros, o recurso será criado e algum tipo de confirmação será exibida. Caso contrário, o formulário é reexibido com mensagen de erro e o processo é repetido.
+  #Em um aplicativ oRails, essas etapas são tratadas convencionalmente pelas ações 'new' e 'create' de um controlador. Vamos adicionar uma implementação típica dessas ações em 'app/controllers/articles_controller.rb, abaixo da ação 'show':
+  
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,11 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   #"get '/articles'" é mapeada para(to) a ação 'index' de 'ArticlesController':
-  get "/articles", to: "articles#index"
-  #Essa 'route'abaixo é outra rota 'get', mas tem algo extra sem seu 'path', o ':id'.Isso designa um parâmetro de rota.Um parâmetro de rota captura um segmento do caminho da solicitação e coloca esse valor no 'Params' Hash, que é acessível pela ação do controlador.Por exemplo, ao lidar com uma solicitação como 'Get http://localhost:3000/articles/1', '1' seria capturado como o valor para ':id', que seria acessível como 'params[:id] na action show de ArticlesContoller.
-  get"/articles/:id", to: "articles#show"
+  #get "/articles", to: "articles#index"
+  
+  #Essa 'route'abaixo é outra rota 'get', mas tem algo extra sem seu 'path', o ':id'.Isso designa um parâmetro de rota.Um parâmetro de rota captura um segmento do caminho da solicitação e coloca esse valor no 'Params' Hash, que é acessível pela ação do controlador.Por exemplo, ao lidar com uma solicitação como 'Get http://localhost:3000/articles/1', '1' seria capturado como o valor para ':id', que seria acessível como 'params[:id]' na action show de ArticlesContoller:
+  #get"/articles/:id", to: "articles#show"
+
+  #O Rails fornece um método de rotas chamado 'resources', que mapeia todas as rotas convencionais para uma coleção de recursos, como artigos. Portanto, antes de prosseguirmos para as seções 'C', 'U' e 'D', vamos substituir as duas rotas 'get' em 'config/routes.rb' por 'resources':
+  resources :articles
 end
