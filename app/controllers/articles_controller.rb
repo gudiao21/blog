033eccb025c27a519b1481e3aedf1557a574706d
gudiao21 <lucianodantas53@gmail.com>
2023-05-18 15:47:38 -0300

    Adicionar arquivos da pasta '/blog'

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -24,6 +24,11 @@ class ArticlesController < ApplicationController
       render :new, status: :unprocessable_entity #Se o 'article' não foi salvo, a 'action' reexibe o formulário renderizando 'app/views/articles/new.html.erb' com o código de status '422 unprocessable Entity'. O título e o corpo aqui são valores fictícios.
     end
   end
+  #Os dados de 'formulário' enviados são colocados no hash de parâmetros, juntamente com os parâmetros de rota capturados. Assim, a ação 'create' pode acessar o 'title' enviado por meio de 'params[:article][:title]' e o 'body' enviado por meio de 'params[:article][:body]'. Poderíamos passar esses valores individualmente para 'Articles.new, mas isso seria detalhado e possivelmente sujeito a erros. E ficaria pior à medida que adicionássemos mais campos.
+  #Passaremos um único Hash que contém os valores. No entanto, ainda devemos especificar quais valores são permitidos nesse Hash. Caso contrário, um usuário mal intencionado poderia enviar campos de formulário extras e sobrescrever dados privados. De fato, se passarmos o Hash 'params[:article]' não filtrado diretamente pára 'Article.new, o Rails gerará um ForbiddenAttributesError para nos alertar sobre o problema. Portanto, usaremos um recurso do Rails chamado 'Strong Parameters' para filtrar os parâmetros. Pense nisso como digitação forte para parâmetros:
+  private def article_params
+    params.require(:article).permit(:title, :body)
+  end
 end
 
 #Obs: 'redirect_to' fará com que o navegador faça uma nova solicitação, enquanto 'render' renderiza a exibição especificada para a solicitação atual. É importante usar o 'redirect_to' depois de alterar o estado do banco de dados ou do aplicativo. Caso contrário, se o usuário atualizar a página, o navegador fará a mesma solicitação e a mutação será repetida.
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,7 @@
 class Article < ApplicationRecord
+    #Como vimos, a criação de um 'resource' é um processo de várias etapas. A manipulação de entrada inválida do usuário é outra etapa desse processo.O 'Rails' fornece um recurso chamado "validations' para nos ajudar a lidar com entradas de usuários inválidas. As 'validations' são regras que são verificadas antes que um objeto de 'model' seja salvo. Se qualquer uma das verificações falhar, o salvamento será interrompido e as mensagens de erro apropriadas serão adicionadas ao atributo errors do objeto modelo."
+    #Vamos adicionar algumas validações ao nosso 'model' em 'app/models/article.rb'
+    validates :title, presence: true #Essa primeira validação declara que um valor de 'title' deve estar presente. Como o título é uma string, isso significa que o valor do 'title' deve conter pelo menos um caractere qu não seja um espaço em branco.
+    validates :body, presence: true, length: { minimum: 10 } #Essa segunda validação declara que um valor de 'body' também deve estar presente. Além disso, ele declara que o valor do 'body' deve ter pelo menos 10 caracteres.
+    #Obs: Você pode estar se perguntando onde os atributos de título e corpo são definidos. O 'Active Record' define automaticamente os atributos do modelo para cada coluna da tabela, portanto, você não precisa declarar esses atributos em seu arquivo de 'model'.
 end
