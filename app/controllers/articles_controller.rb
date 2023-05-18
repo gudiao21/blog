de154dfe80cf346e96901faa9c971ea65cb98dcc
gudiao21 <lucianodantas53@gmail.com>
2023-05-18 17:57:07 -0300

    Adicionar arquivos da pasta '/blog'

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -27,11 +27,32 @@ class ArticlesController < ApplicationController
       render :new, status: :unprocessable_entity #Se o 'article' não foi salvo, a 'action' reexibe o formulário renderizando 'app/views/articles/new.html.erb' com o código de status '422 unprocessable Entity'. O título e o corpo aqui são valores fictícios.
     end
   end
+  
+  #O "update" de um 'resource' é muito semehante à criação de um recurso. Ambos são processos de várias etapas. Primeiro, o usuário solicita um formulário para editar os dados. Em seguida, o usuário envia o formulário. Se não houver erros, o 'resource' será atualizado. Caso contrário, o formulário é reexibido com mensagens de erro e o processo é repetido.
+  #Essas etapas são tratadas convencionalmente pelas ações de 'edit' e 'update' de um controlador. Vamos adicionar uma implementação típica dessas 'actions' a 'app/controllers/articles_controller.rb'
+  #Note que as actions 'edit' e 'update' se assemelham às 'actions' 'new' e 'create'.
+  #A 'action' de 'edit' busca o 'article' do banco de dados e o armazena em '@article' para qu possa ser usado ao criar o formulário. Por padrão, a 'action' de 'edit' renderizará 'app/views/articles/edit.html.erb'.
+  #A 'action' de 'update' (re)busca o artigo do banco de dados e tenta atualizá-lo com os dados do formulário enviado filtrado por 'article_params'. Se nenhuma 'validation' falha e o 'update' for bem-sucedido, a 'action redireciona o navegador para a página do 'article'. Caso contrário, a 'action' reexibe o formulário com mensagens de erro, renderizando assim 'app/views/articles/edit.html.erb'.
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+  
+  def update
+    @article = Article.find(params[:id])
+    
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render :edit, status: :unprocessable_entity
+    end
+
   #Os dados de 'formulário' enviados são colocados no hash de parâmetros, juntamente com os parâmetros de rota capturados. Assim, a ação 'create' pode acessar o 'title' enviado por meio de 'params[:article][:title]' e o 'body' enviado por meio de 'params[:article][:body]'. Poderíamos passar esses valores individualmente para 'Articles.new, mas isso seria detalhado e possivelmente sujeito a erros. E ficaria pior à medida que adicionássemos mais campos.
   #Passaremos um único Hash que contém os valores. No entanto, ainda devemos especificar quais valores são permitidos nesse Hash. Caso contrário, um usuário mal intencionado poderia enviar campos de formulário extras e sobrescrever dados privados. De fato, se passarmos o Hash 'params[:article]' não filtrado diretamente pára 'Article.new, o Rails gerará um ForbiddenAttributesError para nos alertar sobre o problema. Portanto, usaremos um recurso do Rails chamado 'Strong Parameters' para filtrar os parâmetros. Pense nisso como digitação forte para parâmetros:
-  private def article_params
-    params.require(:article).permit(:title, :body)
-  end
+    private
+      def article_params
+        params.require(:article).permit(:title, :body)
+      end
 end
 
 #Obs: 'redirect_to' fará com que o navegador faça uma nova solicitação, enquanto 'render' renderiza a exibição especificada para a solicitação atual. É importante usar o 'redirect_to' depois de alterar o estado do banco de dados ou do aplicativo. Caso contrário, se o usuário atualizar a página, o navegador fará a mesma solicitação e a mutação será repetida.
